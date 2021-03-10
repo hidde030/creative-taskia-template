c5ebcb825dd8240c4d7f3bc96318c02ca388acd3
Hidde Glansdorp <h.glansdorp@gmail.nl>
2021-03-10 17:20:51 +0100

    update

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -16,7 +16,68 @@
                     </form>
                 </div>
                 <div class="col-5 column">
-                  <div class="image"></div>
+                    <div class="image"></div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="tasks">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <h2>Featured Service that We Provide</h2>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 col-6 column">
+                    <div class="task-card">
+                        <img src="{{url('/img/tea.png')}}" alt="tea">
+                        <h4 class="task-card__title">Keep tasks in one place</h4>
+                        <p class="task-card__description">Save time, avoid losing work and information, delegate, and
+                            track tasks to stay on schedule</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-6 column">
+                    <div class="task-card">
+                        <img src="{{url('/img/wallet.png')}}" alt="wallet">
+                        <h4 class="task-card__title">Keep tasks in one place</h4>
+                        <p class="task-card__description">Save time, avoid losing work and information, delegate, and
+                            track tasks to stay on schedule</p>
+                    </div>
+                </div>
+                <div class="col-md-4 col-6 column">
+                    <div class="task-card">
+                        <img src="{{url('/img/pocket-knief.png')}}" alt="knief">
+                        <h4 class="task-card__title">Keep tasks in one place</h4>
+                        <p class="task-card__description">Save time, avoid losing work and information, delegate, and
+                            track tasks to stay on schedule</p>
+                    </div>
+                </div>
+
+
+            </div>
+
+        </div>
+
+    </section>
+    <section class="info-management">
+        <div class="container">
+            <div class="row">
+                <div class="col-12 col-md-6">
+                    <img src="{{url('/img/tablet.png')}}" alt="tablet">
+                </div>
+                <div class="col-12 col-md-6">
+                    <h2>
+                        Why do you need
+                        task management software?
+                    </h2>
+                    <p class="description">
+                        Do you waste time organizing sticky notes, searching your email and apps for to-dos, and
+                        figuring out what to work on first? Then you need one solution to prioritize your tasks, manage
+                        your time, and meet your deadlines.
+                    </p>
+                    <a href="" class="button">Learn more</a>
                 </div>
             </div>
         </div>
